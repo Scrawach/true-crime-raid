36808f944ec860331346754e5529f65ddd5db708
scrawach <scrawach@gmail.com>
2026-03-19 23:23:45 +0300

    fix: remove unused interact label from player hud

--- src/player/player_hud.gd
+++ src/player/player_hud.gd
@@ -1,15 +1,8 @@
 class_name PlayerHUD
 extends CanvasLayer
 
-@onready var interact_label: Label = %"Interact Label"
 @onready var center_container: CenterContainer = %CenterContainer
 @onready var item_handle_tooltip: PanelContainer = %"Item Handle Tooltip"
-
-func show_interact_tooltip() -> void:
-	interact_label.show()
-
-func hide_interact_tooltip() -> void:
-	interact_label.hide()
 
 func show_aim_pointer() -> void:
 	center_container.show()
