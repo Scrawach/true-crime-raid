d10ef999105262b0a7b48819c4388cc166e22bcb
scrawach <scrawach@gmail.com>
2026-03-25 10:19:17 +0300

    feat: make allow get DNA from items only once

--- src/items/interactions/dna_interactive_point_3d.gd
+++ src/items/interactions/dna_interactive_point_3d.gd
@@ -2,7 +2,17 @@ class_name DNAInteractivePoint3D
 extends InteractivePoint3D
 
 @export var dna_data: DNAData
+@export var only_once: bool
+@export var dna_mesh: Node3D
 
 func _on_clicked() -> void:
 	super._on_clicked()
-	disable()
+	
+	if only_once:
+		dispose_dna()
+	else:
+		disable()
+
+func dispose_dna() -> void:
+	dna_mesh.queue_free()
+	queue_free()
