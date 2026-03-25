41e075999fd18b22800277915ede964915c521d0
scrawach <scrawach@gmail.com>
2026-03-25 17:49:34 +0300

    feat: prepared data for first scenario 00_night_fight, add document prototype

--- src/items/data/dna_data.gd
+++ src/items/data/dna_data.gd
@@ -3,5 +3,7 @@ extends Resource
 
 @export var database_name: String
 @export var overlap_percentage: float
+@export_enum("Null", "Partial", "Direct") var match_type: String
+@export_multiline var description: String
 
 var is_processed: bool
