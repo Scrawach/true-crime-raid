3c4688a6377510a5ae901db1aa2a66c3357f3f18
scrawach <scrawach@gmail.com>
2026-03-26 11:48:22 +0300

    feat: show small description on DNA item about from it's item

--- src/items/dna_item.gd
+++ src/items/dna_item.gd
@@ -5,6 +5,8 @@ extends BaseItem
 
 func get_description() -> String:
 	var base_info := data.description
+	if dna_data:
+		base_info += "\n\n%s" % dna_data.description
 	if dna_data.is_processed:
 		base_info += "\n\nРезультаты анализа:\nИмя: %s\nСовпадение: %.2f%%" % [dna_data.database_name, dna_data.overlap_percentage]
 	return base_info
