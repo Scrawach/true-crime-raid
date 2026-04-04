5c20e6f6a5b5d343d81cd7b0f05996ec5366818f
scrawach <scrawach@gmail.com>
2026-04-04 17:18:07 +0300

    feat: introduces to table implementation

---
+++ src/devices/table/interact_with_table.gd
@@ -0,0 +1,9 @@
+class_name InteractWithTable
+extends InteractWithDevice
+
+
+func start_interaction(target: PlayerBody3D) -> void:
+	super.start_interaction(target)
+
+func stop_interaction() -> void:
+	super.stop_interaction()
