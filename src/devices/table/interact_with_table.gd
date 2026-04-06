cda3a3d514878e54e939faa90e2328e3315c93fc
scrawach <scrawach@gmail.com>
2026-04-06 11:33:38 +0300

    feat: update laboratory, implement found stickers on table

--- src/devices/table/interact_with_table.gd
+++ src/devices/table/interact_with_table.gd
@@ -4,6 +4,7 @@ extends InteractWithDevice
 @export var sticker_hand: StickerHand
 @export var label_3d: Label3D
 @export var table_sticker_dragging: DragAndDrop3D
+@export var initializer: TableInitializer
 
 func _ready() -> void:
 	camera_move_stopped.connect(_on_camera_stopped)
@@ -11,6 +12,7 @@ func _ready() -> void:
 func start_interaction(target: PlayerBody3D) -> void:
 	super.start_interaction(target)
 	label_3d.show()
+	label_3d.text = "Найдено элементов: %s" % initializer.get_found_string()
 	table_sticker_dragging.enable()
 
 func stop_interaction() -> void:
--- src/devices/table/table_initializer.gd
+++ src/devices/table/table_initializer.gd
@@ -5,15 +5,38 @@ extends Node
 @export var hand: StickerHand
 @export var sticker_scene: PackedScene
 
+var all_stickers: Array[TableStickerData]
+var found_stickers: Array[TableStickerData]
+
 func _ready() -> void:
+	GameManager.keyword_found.connect(_on_keyword_found)
+	GameManager.dna_investigated.connect(_on_dna_investigated)
 	initialize()
 
+func _on_keyword_found(keyword: KeywordData) -> void:
+	for sticker in all_stickers:
+		if sticker.keyword_id == keyword.id and not sticker in found_stickers:
+			found_stickers.append(sticker)
+			make_sticker(sticker)
+
+func _on_dna_investigated(dna: DNAData) -> void:
+	for sticker in all_stickers:
+		if sticker.dna_id == dna.id and not sticker in found_stickers:
+			found_stickers.append(sticker)
+			make_sticker(sticker)
+
 func initialize() -> void:
-	for data in load_all_stickers_data(directory_path):
-		var sticker := sticker_scene.instantiate() as Sticker
-		hand.add_child(sticker)
-		sticker.initialize(data)
+	all_stickers = load_all_stickers_data(directory_path)
+
+func get_found_string() -> String:
+	return "%s / %s" % [found_stickers.size(), all_stickers.size()]
+
+func make_sticker(data: TableStickerData) -> Sticker:
+	var instance := sticker_scene.instantiate() as Sticker
+	hand.add_child(instance)
 	hand.update_sticker_positions()
+	instance.initialize(data)
+	return instance
 
 static func load_all_stickers_data(directory: String) -> Array[TableStickerData]:
 	const RESOURCE_FORMAT := "tres"
