ae1903911e6d85f0db7234057a8d2c456a8ca76b
scrawach <scrawach@gmail.com>
2026-04-11 00:38:48 +0300

    feat: last update with models setup

--- src/devices/trash/interact_with_trash_holder.gd
+++ src/devices/trash/interact_with_trash_holder.gd
@@ -1,23 +1,12 @@
 class_name InteractWithTrashHolder
 extends Node
 
-const OPEN_ANIMATION := &"open"
-
 @export var trash_scene: PackedScene
 @export var interaction: Area3D
 @export var destroy_area: Area3D
-@export var animation: AnimationPlayer
 
 func _ready() -> void:
-	interaction.body_entered.connect(_on_body_entered)
-	interaction.body_exited.connect(_on_body_exited)
 	destroy_area.body_entered.connect(_on_body_destroy_entered)
-
-func _on_body_entered(_body: Node3D) -> void:
-	animation.play(OPEN_ANIMATION)
-
-func _on_body_exited(_body: Node3D) -> void:
-	animation.play_backwards(OPEN_ANIMATION)
 
 func _on_body_destroy_entered(body: Node3D) -> void:
 	if body.scene_file_path != trash_scene.resource_path:
